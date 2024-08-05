47a3b247a3a4b28b3dcfb247f23babe970eb491f
k15460 <k15460@ktkbank.com>
2024-08-05 11:37:00 +0530

    basic DB Creation

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
+
+dotEnv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=>console.log("DB Connected"))
+.catch((err)=>console.log(err));
+
+app.listen(process.env.PORT,()=>console.log("Server is running"))
