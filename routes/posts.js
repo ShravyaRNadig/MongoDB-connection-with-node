9ee902185e7ce68c5249c909693c0783356aa609
k15460 <k15460@ktkbank.com>
2024-08-05 12:35:16 +0530

    Delete API

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Post = require("../model/post")
 
+// Post API New post
 router.post("/newPost", async (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
@@ -12,28 +13,38 @@ router.post("/newPost", async (req, res) => {
     res.json(savedPost);
 });
 
-
+// Get Api get All post
 router.get("/getPosts", async (req, res) => {
     const posts = await Post.find({});
     res.json(posts);
 });
 
+// Get Api get post by ID
 router.get("/getPostsById/:postid", async (req, res) => {
     const _id = req.params.postid;
     const post = await Post.findById(_id);
     res.json(post);
 });
 
-router.patch("/editDescp/:postid",async (req, res) => {
-    const _id = req.params.postid;
-    await Post.findByIdAndUpdate(_id,{$set:{description: req.body.description}});
-    res.json({"Status": "Description Updated"});
-})
-
-router.put("/edit/:postid",async (req, res) => {
+// Edit API edit post by ID
+router.put("/editPost/:postid",async (req, res) => {
     const _id = req.params.postid;
     await Post.findByIdAndUpdate(_id,{$set:{title: req.body.title ,description: req.body.description}});
     res.json({"Status": "Post Updated"});
-})
+});
+
+// Patch API patch post by ID
+router.patch("/patchDescp/:postid",async (req, res) => {
+    const _id = req.params.postid;
+    await Post.findByIdAndUpdate(_id,{$set:{description: req.body.description}});
+    res.json({"Status": "Description Updated"});
+});
+
+// Delete API Delete one post by ID
+router.delete("/deleteOnePost/:postid",async (req, res) => {
+    const _id = req.params.postid;
+    await Post.deleteOne({_id: _id});
+    res.json({"Status": "1 Post Deleted"});
+});
 
 module.exports = router;
